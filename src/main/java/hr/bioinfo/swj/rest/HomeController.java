03f9c9dc5f317ab8870b3f531e7a13878d4c21c2
jdiminic <jdiminic@gmail.com>
2025-04-18 14:35:55 +0200

    save all

---
+++ src/main/java/hr/bioinfo/swj/rest/HomeController.java
@@ -0,0 +1,42 @@
+package hr.bioinfo.swj.rest;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        log.info("HomeController.index() called");
+        model.addAttribute("title", "Home - SpringWebJobs");
+        model.addAttribute("pageTitle", "Welcome to SpringWebJobs");
+        model.addAttribute("activeTab", "home");
+        model.addAttribute("content", "index :: content");
+        return "index";
+    }
+
+    @GetMapping("/jobs")
+    public String jobs(Model model) {
+        log.info("HomeController.jobs() called");
+        model.addAttribute("title", "Jobs - SpringWebJobs");
+        model.addAttribute("pageTitle", "Job Management");
+        model.addAttribute("activeTab", "jobs");
+        model.addAttribute("content", "jobs :: content");
+        return "jobs";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        log.info("HomeController.about() called");
+        model.addAttribute("title", "About - SpringWebJobs");
+        model.addAttribute("pageTitle", "About SpringWebJobs");
+        model.addAttribute("activeTab", "about");
+        model.addAttribute("content", "about :: content");
+        return "about";
+    }
+}
---
+++ src/main/java/hr/bioinfo/swj/rest/JobRestController.java
@@ -0,0 +1,12 @@
+package hr.bioinfo.swj.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JobRestController {
+
+
+
+
+
+}
